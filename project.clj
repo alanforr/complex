dab11ea2faf4f75962a2a8a1a73a1f3a0e8fff54
alanforr <alanmichaelforrester@googlemail.com>
2015-06-01 08:20:30 +0100

    Put criterium as a dev dependency.

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject complex "0.1.1"
+(defproject complex "0.1.2"
   :description "A Clojure library for doing fast complex number calculations that
 wraps the Java commons-math3 Complex library."
   :url "https://github.com/alanforr/complex"
   :uberjar {:aot :all}
+  :dev {:dependencies  [criterium "0.4.3"]}
   :scm {:name "git"
             :url "https://github.com/alanforr/complex"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.commons/commons-math3 "3.5"]
-                 [criterium "0.4.3"]]
+                 [org.apache.commons/commons-math3 "3.5"]]
   :main complex.core)
