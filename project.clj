a8d0c9f45b95496b6ca1a5450b771d6e9edad32e
alanforr <alanmichaelforrester@googlemail.com>
2015-08-20 12:07:56 +0100

    Change version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject complex "0.1.6"
+(defproject complex "0.1.7"
   :description "A Clojure library for doing fast complex number calculations that
 wraps the Java commons-math3 Complex library."
   :url "https://github.com/alanforr/complex"
