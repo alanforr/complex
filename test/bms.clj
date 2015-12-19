15fd203c90f40dff39d62d6662c601bbce14f149
Dany Haddad <danyhaddad43@gmail.com>
2015-12-19 16:52:35 -0500

    Fixed namespace and organization of tests

--- test/bms.clj
+++ test/bms.clj
@@ -1,6 +1,6 @@
-(ns complex.core.test-ns
+(ns bms
   (:require [criterium.core :as c])
-  (:use [complex.core :exclude [+ - * /]]
+  (:use [complex.core]
         [clojure.test]))
 
 (def c1 (complex 1 2))
@@ -8,39 +8,29 @@
 c1
 c2
 
-(deftest test-equals
-  (is (true?
-        (equals (complex 1) (complex 1))))
-  (is (true?
-        (equals (complex 0.0) (complex 0.0))))
-  (is (true?
-        (equals (complex -0.0) (complex 0.0) 0.1)))
-  (is (false?
-        (equals (complex -0.0) (complex 0.0)))))
+(c/bench (dotimes [_ 1000] (+ c1 c2)))
+(c/bench (dotimes [_ 1000] (+ c1 c2 2)))
+(c/bench (dotimes [_ 1000] (+ c1 c2 2 c1 c1 c1)))
+(c/bench (dotimes [_ 1000] (+ c1 c2 2 c1 c1 c1 c2 c2 c2)))
+(c/bench (dotimes [_ 1000] (+ c1 c2 2 c1 c1 c1 c2 c2 c2 c1 c1 c1)))
 
-  (c/bench (dotimes [_ 1000] (+ c1 c2)))
-  (c/bench (dotimes [_ 1000] (+ c1 c2 2)))
-  (c/bench (dotimes [_ 1000] (+ c1 c2 2 c1 c1 c1)))
-  (c/bench (dotimes [_ 1000] (+ c1 c2 2 c1 c1 c1 c2 c2 c2)))
-  (c/bench (dotimes [_ 1000] (+ c1 c2 2 c1 c1 c1 c2 c2 c2 c1 c1 c1)))
+(c/bench (dotimes [_ 1000] (* c1 c2)))
+(c/bench (dotimes [_ 1000] (* c1 c2 2)))
+(c/bench (dotimes [_ 1000] (* c1 c2 2 c1 c1 c1)))
+(c/bench (dotimes [_ 1000] (* c1 c2 2 c1 c1 c1 c2 c2 c2)))
+(c/bench (dotimes [_ 1000] (* c1 c2 2 c1 c1 c1 c2 c2 c2 c1 c1 c1)))
 
-  (c/bench (dotimes [_ 1000] (* c1 c2)))
-  (c/bench (dotimes [_ 1000] (* c1 c2 2)))
-  (c/bench (dotimes [_ 1000] (* c1 c2 2 c1 c1 c1)))
-  (c/bench (dotimes [_ 1000] (* c1 c2 2 c1 c1 c1 c2 c2 c2)))
-  (c/bench (dotimes [_ 1000] (* c1 c2 2 c1 c1 c1 c2 c2 c2 c1 c1 c1)))
+(c/bench (dotimes [_ 1000] (- c1 c2)))
+(c/bench (dotimes [_ 1000] (- c1 c2 2)))
+(c/bench (dotimes [_ 1000] (- c1 c2 2 c1 c1 c1)))
+(c/bench (dotimes [_ 1000] (- c1 c2 2 c1 c1 c1 c2 c2 c2)))
+(c/bench (dotimes [_ 1000] (- c1 c2 2 c1 c1 c1 c2 c2 c2 c1 c1 c1)))
 
-  (c/bench (dotimes [_ 1000] (- c1 c2)))
-  (c/bench (dotimes [_ 1000] (- c1 c2 2)))
-  (c/bench (dotimes [_ 1000] (- c1 c2 2 c1 c1 c1)))
-  (c/bench (dotimes [_ 1000] (- c1 c2 2 c1 c1 c1 c2 c2 c2)))
-  (c/bench (dotimes [_ 1000] (- c1 c2 2 c1 c1 c1 c2 c2 c2 c1 c1 c1)))
+(c/bench (dotimes [_ 1000] (/ c1 c2)))
+(c/bench (dotimes [_ 1000] (/ c1 c2 2)))
+(c/bench (dotimes [_ 1000] (/ c1 c2 2 c1 c1 c1)))
+(c/bench (dotimes [_ 1000] (/ c1 c2 2 c1 c1 c1 c2 c2 c2)))
+(c/bench (dotimes [_ 1000] (/ c1 c2 2 c1 c1 c1 c2 c2 c2 c1 c1 c1)))
 
-  (c/bench (dotimes [_ 1000] (/ c1 c2)))
-  (c/bench (dotimes [_ 1000] (/ c1 c2 2)))
-  (c/bench (dotimes [_ 1000] (/ c1 c2 2 c1 c1 c1)))
-  (c/bench (dotimes [_ 1000] (/ c1 c2 2 c1 c1 c1 c2 c2 c2)))
-  (c/bench (dotimes [_ 1000] (/ c1 c2 2 c1 c1 c1 c2 c2 c2 c1 c1 c1)))
-
-  (c/bench (dotimes [_ 1000] (sin c1)))
-  (c/bench (dotimes [_ 1000] (sin 1)))
+(c/bench (dotimes [_ 1000] (sin c1)))
+(c/bench (dotimes [_ 1000] (sin 1)))
