8f7cd66d0cdf7a0e451532a643504cf913bc81f6
alanforr <alanmichaelforrester@googlemail.com>
2017-12-26 21:59:05 +0000

    trying to deploy without signing.

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@ wraps the Java commons-math3 Complex library."
   :url "https://github.com/alanforr/complex"
   :uberjar {:aot :all}
   :profiles {:dev {:dependencies  [[criterium "0.4.3"]]}}
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
   :scm {:name "git"
             :url "https://github.com/alanforr/complex"}
   :license {:name "Eclipse Public License"
